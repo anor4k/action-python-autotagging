6d6cf9635a85ad4c4d9794817ee602d179e937c4
jorrick <jorrick.sleijster@adyen.com>
2022-07-22 16:08:06 +0200

    FIX broken main.py

--- main.py
+++ main.py
@@ -9,13 +9,16 @@ def main():
     with Path(os.environ["INPUT_PATH"]).open() as f:
         exec(f.read(), about)
 
-    prefix = os.environ["INPUT_PREFIX"]
+    prefix, suffix = os.environ["INPUT_PREFIX"], os.environ["INPUT_SUFFIX"]
     variable = os.environ["INPUT_VARIABLE"]
-    suffix = os.environ["INPUT_SUFFIX"]
     version_tag = f"{prefix}{about[variable]}{suffix}"
+    secret_token = os.environ["INPUT_TOKEN"]
+    repository = os.environ["GITHUB_REPOSITORY"]
 
-    g = Github(os.environ["INPUT_TOKEN"])
-    repo = g.get_repo(os.environ["github"]["repository"])
+    print(f"Configuration: {version_tag=}, {repository=}, {len(secret_token)=}")
+
+    g = Github(secret_token)
+    repo = g.get_repo(repository)
 
     for tag in repo.get_tags():
         if tag.name == version_tag:
